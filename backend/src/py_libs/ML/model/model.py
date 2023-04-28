4c8f47b2b79ba8b562d39d2f6b6b1d5c43f686c9
tw40210 <tw40210@gmail.com>
2023-04-28 19:43:21 +0100

    feat: profit loss, norm layers

--- backend/src/py_libs/ML/model/model.py
+++ backend/src/py_libs/ML/model/model.py
@@ -19,12 +19,14 @@ class GRUNet(nn.Module):
         self.gru = nn.GRU(in_dim, 256, 2)
         self.fc2 = nn.Linear((window_len-2)*256, 512)
         self.fc3 = nn.Linear(512, out_dim)
+        self.bn1=nn.BatchNorm1d(512)
+        self.ln1=nn.LayerNorm(256)
 
     def forward(self, x):
         # x = x.reshape(x.shape[0],-1)
         x, _ = self.gru(x)
-        x = nn.functional.relu(x)
+        x = nn.functional.relu(self.ln1(x))
         x = x.reshape(x.shape[0], -1)
-        x = nn.functional.relu(self.fc2(x))
+        x = nn.functional.relu(self.bn1(self.fc2(x)))
         x = self.fc3(x)
         return x
