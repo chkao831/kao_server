84a2ed7b1f5202b977ae1d64a7984a9e58ae2f6c
tw40210 <tw40210@gmail.com>
2023-12-23 11:07:06 +0000

    init push; draft folder structure

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,1 +1,13 @@
-REPO = __file__[:__file__.find("src")]
+import datetime
+
+from omegaconf import OmegaConf
+
+REPO = __file__[: __file__.find("src")]
+
+
+def datetime_resolver(time):
+    datetime_format = "%Y-%m-%d %H:%M:%S"
+    return datetime.datetime.strptime(time, datetime_format)
+
+
+OmegaConf.register_new_resolver("time", datetime_resolver())
