7375ae066c2f33b449db04e03a2eac70db3291bf
tw40210 <tw40210@gmail.com>
2023-12-25 19:47:44 +0000

    WIP: simple strategy

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from omegaconf import OmegaConf
 
 REPO = Path(__file__[: __file__.find("src")])
+START_TIMESTAMP = datetime.datetime(2000, 1, 1)
 
 
 def datetime_resolver(time):
