a38089de4ed105773e07beb0486b0ad76c22c4f5
tw40210 <tw40210@gmail.com>
2023-12-26 20:16:43 +0000

    ref: remove unused

--- backend/src/py_libs/strategies/simple_strategy.py
+++ backend/src/py_libs/strategies/simple_strategy.py
@@ -1,8 +1,6 @@
-import pickle
 from abc import ABC, abstractmethod
 
 import numpy as np
-import src
 
 
 class BasicStrategy(ABC):
@@ -20,10 +18,6 @@ class SimpleStrategy(BasicStrategy):
         super().__init__()
         self.historical_data = historical_data
         self.biases = {symbol: 0 for symbol in historical_data.keys()}
-        with open(
-            src.REPO / "tests/test_data/test_historical_data/test_two_historical_data.pkl", "wb"
-        ) as f:
-            pickle.dump(historical_data, f)
 
     def select_action(self, state):
         pass
