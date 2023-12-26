f9b54c4eefbf3060cb1677d0b35e2ec077f6f627
tw40210 <tw40210@gmail.com>
2023-12-26 20:16:06 +0000

    ref: remove unused

--- backend/src/py_libs/alpaca_stock/alpaca_main.py
+++ backend/src/py_libs/alpaca_stock/alpaca_main.py
@@ -12,52 +12,27 @@
 # =============== DataStream
 
 
-from datetime import datetime
+from pathlib import Path
 
-# ===== websocket
-# import websocket, json
-#
-# def on_open(ws):
-#     print("opened")
-#     auth_data = {
-#         "action": "auth",
-#         "data": {"key_id":'}
-#     }
-#
-#     ws.send(json.dumps(auth_data))
-#
-#     listen_message = {"action":"subscribe", "quotes":["AMD"]}
-#
-#     ws.send(json.dumps(listen_message))
-#
-#
-# def on_message(ws, message):
-#     print("received a message")
-#     print(message)
-#
-#
-# def on_close(ws):
-#     print("closed connection")
-#
-#
-# socket = 'wss://stream.data.sandbox.alpaca.markets/v2/iex'
-#
-# ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
-# ws.run_forever()
 import hydra
-from src.py_libs.alpaca_stock.data_retriever import AlpacaDataRetriever
+from src.py_libs.strategies.simple_strategy import SimpleStrategy
+from src.py_libs.utils.data_process import load_historical_data
 
 
 def main():
     with hydra.initialize(version_base=None, config_path="../../../src/config"):
         config = hydra.compose(config_name="main")
 
-    data_retriever = AlpacaDataRetriever()
-    data_retriever.get_historical_data(
-        ["AAPL"], datetime(2021, 12, 1), datetime(2023, 12, 1), save_file=True
-    )
-    data_retriever.update_all_in_data_dir()
+    # data_retriever = AlpacaDataRetriever()
+    # data_retriever.get_historical_data(
+    #     ["GOOG"], datetime(2021, 12, 1), datetime(2023, 12, 1), save_file=True
+    # )
+    # data_retriever.update_all_in_data_dir()
 
+    all_historical_data = load_historical_data(Path("output/historical_data"))
+    simple_strategy = SimpleStrategy(all_historical_data)
+
+    print(simple_strategy)
     print(config)
 
 
